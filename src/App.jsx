26266d08ec5577405e519625cfb8a1f56382b109
Brito Reji <britoreji2006@gmail.com>
2024-01-13 13:35:00 +0530

    added slider and demo image

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import Header from './components/Header/Header'
-
+import Home from './components/Home/Home'
 import './App.css'
 import MenuBar from './components/MenuBar/MenuBar'
 function App() {
   return (
     <div>
       <Header />
-      
-     
       <MenuBar/>
+     <Home/> 
+     
+
+
      <div className="lg">
       <h1>OPEN WITH YOUR MOBILE KID 🗿</h1>
      </div>
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,13 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/effect-cards';
+import './styles.css';
+import { EffectCards, Autoplay } from 'swiper/modules';
 
-function Home() {
+export default function App() {
+  let [s,ds] = useState([1,2,3,4,5,6])
   return (
-    <div>
-        <div className="movie-banner">
-            
-        </div>
-    </div>
-  )
-}
+    <>
+      <Swiper
+        effect={'cards'}
+        grabCursor={true}
+        modules={[EffectCards, Autoplay]} // Include Autoplay module
+        autoplay={{ delay: 3000} }// Autoplay settings
+        className="mySwiper"
+      >
+        {/* <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide> */}
 
-export default Home
+        {
+          s.map((i)=>{
+            return <SwiperSlide  key={i}>Slide {i}</SwiperSlide>
+          })
+        }
+
+      </Swiper>
+    </>
+  );
+}

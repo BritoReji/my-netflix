cf31a23b8db29db01f3156febd3ad6149d394755
Brito Reji <britoreji2006@gmail.com>
2024-01-15 18:56:42 +0530

    some bug fix

--- src/components/Home/NewMovies/NewMovies.jsx
+++ src/components/Home/NewMovies/NewMovies.jsx
@@ -9,7 +9,7 @@ function NewMovies({movies}) {
         <div className="new-movies">
             {
                 movies.map((item,index)=>{
-                    return <img key={index} src={`https://image.tmdb.org/t/p/w200${item.poster_path}`} alt="" srcset="" />
+                    return <img key={index} className='n-mov' src={`https://image.tmdb.org/t/p/w200${item.poster_path}`} alt="" srcset="" />
                 })
             }
             

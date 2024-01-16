b1a7c9745d5046246d2bee67e5ec10c6d0734b4f
Brito Reji <britoreji2006@gmail.com>
2024-01-16 21:56:03 +0530

    banner update

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,11 +3,13 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/effect-cards';
 import './styles.css';
-import { EffectCards, Autoplay } from 'swiper/modules';
-
+import { Navigation, Pagination, Mousewheel, Keyboard,Autoplay } from 'swiper/modules';
 import axios from 'axios';
 import NewMovies from './NewMovies/NewMovies';
 import './Home.css'
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
 ;
 const apiKey = import.meta.env.VITE_API_KEY;
@@ -40,23 +42,33 @@ export default function App() {
   return (
     <>
       <Swiper
-        effect={'cards'}
-        grabCursor={true}
-        modules={[EffectCards, Autoplay]}
-        autoplay={{ delay: 3000 }}
-        className="mySwiper"
-        height={1}
+      style={{
+        "--swiper-theme-color":"red",
         
-        
+      }}
+         cssMode={true}
+         modules={[Navigation, Pagination, Mousewheel, Keyboard, Autoplay]}
+         navigation={true}
+         pagination={true}
+         mousewheel={true}
+         keyboard={true}
+         autoplay={{ delay: 3000 }} // Set the autoplay delay
+         loop={true}
+         className="mySwiper"
       >
         {s
         
         .map((item, index) => (
-          <SwiperSlide key={index} 
-          style={{backgroundImage: `url(https://image.tmdb.org/t/p/w500/${item.poster_path})`}}
+          <div className="gra" key={index}>
+         <SwiperSlide key={index} 
+          style={{backgroundImage: `url(https://image.tmdb.org/t/p/original/${item.backdrop_path})`,
+        
+        }}
           >
             
           </SwiperSlide>
+          </div>
+         
         ))}
        
       </Swiper>
